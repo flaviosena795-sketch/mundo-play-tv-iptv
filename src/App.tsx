7f9e568ec1751dc2999c97a6bfc1ade5926cb15c
flaviosena795-sketch <flaviosena795@gmail.com>
2025-10-15 00:41:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,34 @@
-import '../styles/globals.css'; import Script from 'next/script'; function MyApp({ Component, pageProps }) { return ( <> {/* Google Analytics 4 */} <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-MF1GH610V8" /> <Script id="gtag-init" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: window.dataLayer = window.dataLayer || [];
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = 'G-MF1GH610V8';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            if (!window.dataLayer) window.dataLayer = [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', { page_path: window.location.pathname });
+          `,
+        }}
+      />
+      
+      {/* Resto do site */}
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
