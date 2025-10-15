271c02e32da94110f00e3df5f1775ba236ee8800
flaviosena795-sketch <flaviosena795@gmail.com>
2025-10-15 00:57:41 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Script from 'next/script';
+import React from 'react';
 
-const GA_MEASUREMENT_ID = 'G-MF1GH610V8';
-
-function MyApp({ Component, pageProps }: AppProps) {
+function App() {
   return (
-    <>
-      {/* Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            if (!window.dataLayer) window.dataLayer = [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', { page_path: window.location.pathname });
-          `,
-        }}
-      />
-      
-      {/* Resto do site */}
-      <Component {...pageProps} />
-    </>
+    <div>
+      {/* Seu site */}
+      <h1>Olá Mundo Play TV</h1>
+    </div>
   );
 }
 
-export default MyApp;
+export default App;
