c6e1c4591337b0bdce237ee17790a30dcd8e0f35
flaviosena795-sketch <flaviosena795@gmail.com>
2025-10-15 01:17:52 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       {/* Conteúdo original do seu site */}
       <h1>Bem-vindo ao Mundo Play TV</h1>
-      {/* Aqui você pode colocar todo o conteúdo que tinha antes */}
+      <p>Aqui você pode adicionar todo o conteúdo que tinha antes.</p>
     </div>
   );
 }
